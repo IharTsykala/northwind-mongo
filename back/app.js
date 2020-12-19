2008c9879fc7263e920443866653b8e05e7fa0fe
Ihar Tsykala <Sudja09@bk.ru>
2020-12-19 22:02:34 +0300

    feat: clone bot

---
+++ back/app.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const fs = require('fs')
+
+console.log('hi2')
+console.log(__dirname)
+
+console.log(path.basename(__filename))
+// console.log(path.dirname(__filename))
+
